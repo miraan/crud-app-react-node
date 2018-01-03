18da87a90464c6b29c067e0dc463efdfb4993849
Miraan Tabrez <miraan100@yahoo.co.uk>
2018-01-03 12:16:09 +0000

    Add redux, react-redux, redux-thunk and configureStore function

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
+const store = configureStore()
+
 const rootElement = document.querySelector('root')
 if (!(rootElement instanceof Element)) {
   throw new Error('Invalid root element type')
 }
 
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(
+  <App />,
+  rootElement
+)
 registerServiceWorker()
